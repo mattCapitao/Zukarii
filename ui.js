1e60dd1dd0d6157bba087f7b8e7129a2cfe66391
Matt Capitao <matt.capitao@gmail.com>
2025-02-24 14:12:46 -0500

    moved adventure log functions to UI

--- ui.js
+++ ui.js
@@ -134,3 +134,22 @@ function render() {
     state.lastProjectileY = state.projectile ? state.projectile.y : null;
     state.needsInitialRender = false;
 }
+
+const adventureLog = {
+    entries: [],
+    maxEntries: 20
+};
+
+function writeToLog(message) {
+    adventureLog.entries.unshift(message); // Add to the top
+    if (adventureLog.entries.length > adventureLog.maxEntries) {
+        adventureLog.entries.pop(); // Remove from the bottom
+    }
+}
+
+function clearLog() {
+    adventureLog.entries = [];
+}
+
+window.writeToLog = writeToLog;
+window.clearLog = clearLog;
