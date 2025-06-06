8c585039ccdcbbdbee1fbdf013c6ca4cd84599a9
Matt Capitao <matt.capitao@gmail.com>
2025-06-06 15:13:32 -0400

    added perfoamance tag

--- Game.js
+++ Game.js
@@ -367,20 +367,35 @@ export class Game {
         let fps = 0;
         let lastFpsUpdate = performance.now();
         const fpsCounter = document.getElementById('fps-counter');
+        let perfLabel = {};
+        if (window.location.host === 'game.zukarii.com') {
+            perfLabel = { host: 'game.zukarii.com', env: 'Pre-Alpha: prod 02: xxx.xxx.xxx.156:443' };
+        } else {
+            perfLabel = { host: window.location.host, env: 'Dev: dev 01: 127.0.0.1:3000' };
+        }
 
         const gameLoop = (currentTime) => {
             this.gameLoopId = requestAnimationFrame(gameLoop);
             const deltaTime = (currentTime - lastTime) / 1000;
             lastTime = currentTime;
 
+            //environment check
+        
+
             // FPS calculation
             frameCount++;
             if (currentTime - lastFpsUpdate > 1000) {
+
+                const currentDate = new Date();
+                const year = currentDate.getFullYear(); const month = String(currentDate.getMonth() + 1).padStart(2, '0'); const day = String(currentDate.getDate()).padStart(2, '0');
+                const hours = String(currentDate.getHours()).padStart(2, '0'); const minutes = String(currentDate.getMinutes()).padStart(2, '0'); const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+                const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} `;
+
                 fps = frameCount;
                 frameCount = 0;
                 lastFpsUpdate = currentTime;
                 if (fpsCounter) {
-                    fpsCounter.textContent = `FPS: ${fps}`;
+                    fpsCounter.textContent = ` - Host: ${perfLabel.host} | Env: ${perfLabel.env} | Now: ${formattedDate} | FPS: ${fps}`;
                 }
             }
 
